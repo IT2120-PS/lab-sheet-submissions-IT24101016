368f697cf18dd69ac9481fa1fa622e91d0678629
IT24101016 <IT24101016@my.sliit.lk>
2025-10-23 09:34:35 +0530

    IT24101016_Lab_10

---
+++ Lab Sheet 10/IT24101016.R
@@ -0,0 +1,20 @@
+setwd("C:\\Users\\jayas\\Desktop\\IT24101016")
+getwd()
+
+##Q1)
+snack_types <- c("A", "B", "C", "D")
+observed <- c(120, 95, 85, 100)
+
+##Q2)
+expected_prob <- c(0.25, 0.25, 0.25, 0.25)
+
+##Q3)
+test_result <- chisq.test(x = observed, p = expected_prob)
+
+
+print("Chi-Square Goodness-of-Fit Test Results:")
+print(test_result)
+
+
+print("Expected counts:")
+print(test_result$expected)
